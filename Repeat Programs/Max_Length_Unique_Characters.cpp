56377b46c353fe2aa146b9496b0cbd204ff009cc
Yashchaudhari-12 <chaudhariyash3006@gmail.com>
2026-03-28 21:32:34 +0530

    "Day 24 - Maintained Consistency"

---
+++ Repeat Programs/Max_Length_Unique_Characters.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int max_len1(string s)
+{
+    unordered_set<char>st;
+
+    int i=0,max_len=0;
+
+    for(int j=0;j<s.length();j++)
+    {
+        while(st.find(s[j])!=st.end())
+        {
+            st.erase(s[i]);
+            i++;
+        }
+        st.insert(s[j]);
+
+         max_len=max(max_len,j-i+1);
+    }
+    return max_len;
+}
+int main()
+{
+    string s;
+
+    cout << "Enter String : ";
+    getline(cin,s);
+
+    int max_len = max_len1(s);
+
+    cout << "The Max Length With Unique Characters Is : " << max_len;
+    return 0;
+}
